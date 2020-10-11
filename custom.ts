f558cb076c5b61cfc61de887ecf57ded076ffe65
Peter Heldens <peter.heldens@live.nl>
2020-10-11 20:27:43 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -24,6 +24,7 @@ namespace C2D {
         basic.clearScreen()
         who()
     }
+    //% block
     export function unregister_device () {
         basic.clearScreen()
         if (identity >= 0) {
